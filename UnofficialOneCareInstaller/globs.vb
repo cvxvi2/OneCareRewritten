a64cfbd971b1e8a816fc4ca55ea2e69d72230510
Daniel Oakley <danieloakley34@hotmail.co.uk>
2022-05-22 17:59:16 +0100

    Move langpacks to separate module, add debug line entry to log function.

--- UnofficialOneCareInstaller/globs.vb
+++ UnofficialOneCareInstaller/globs.vb
@@ -7,56 +7,12 @@
     Public discVersion As String = "0.00"
     Public isDebugBuild As Boolean = True
     Public bypassOSCheck As Boolean = False
+    Public instLangCode = "en-gb"
     Public Sub log(ByVal data As String)
         Try
             insttlog.TextBox1.AppendText(Environment.NewLine & data.ToString)
+            Debug.WriteLine(data.ToString)
         Catch ex As Exception
         End Try
     End Sub
-
-
-    Function getLangText(ByVal langcode As String, ByVal formname As String, ByVal reqlbl As String)
-        Dim errtxt As String = "An error occurred loading the language pack."
-        Select Case langcode
-            Case "en-gb"
-                Select Case formname
-                    Case "preinst"
-                        Select Case reqlbl
-                            Case "Label2"
-                                Return "Welcome to the preinstallation environment for Windows Live OneCare Rewritten by Cobs." & Environment.NewLine & Environment.NewLine &
-                                    "Installation Notes:" & Environment.NewLine & Environment.NewLine &
-                                    "-If you're attempting a reinstall after a failed initial install, please delete %temp%\OCSetup" & Environment.NewLine &
-                                    "and c:\OneCare" & Environment.NewLine &
-                                    "-Windows XP is only supported with 2.5 discs on SP3." & Environment.NewLine &
-                                    "-Backup is only functional on V1.5 Gold discs." & Environment.NewLine & Environment.NewLine & Environment.NewLine &
-                                    "Requirements:" & Environment.NewLine &
-                                    "-Microsoft .Net 4.0" & Environment.NewLine &
-                                    "-Windows Vista or Windows XP 32-Bit (SP3)" & Environment.NewLine &
-                                    "-Windows Live ID Assistant Service" & Environment.NewLine &
-                                    "-Windows Live OneCare disc in optical drive or virtually mounted"
-                            Case Else
-                                Return errtxt
-                        End Select
-                    Case Else
-                        Return errtxt
-                End Select
-
-
-            Case "fr-fr"
-                Select Case formname
-                    Case "preinst"
-                        Select Case reqlbl
-                            Case "label2"
-                            Case Else
-                                Return errtxt
-                        End Select
-                    Case Else
-                        Return errtxt
-                End Select
-            Case Else
-                Return errtxt
-        End Select
-
-
-    End Function
 End Module
---
+++ UnofficialOneCareInstaller/langpacks.vb
@@ -0,0 +1,48 @@
+﻿Module langpacks
+    Function getLangText(ByVal langcode As String, ByVal formname As String, ByVal reqlbl As String)
+        'This is for the installer only and won't effect the actual installation, however, the langcode will be changed to install respective packages for Onecare.
+        Dim errtxt As String = "An error occurred loading the language pack."
+        log("[getLangText] Loading language pack code: " & langcode & " for " & formname & " requesting " & reqlbl)
+        Select Case langcode
+            Case "en-gb"
+                Select Case formname
+                    Case "preinst"
+                        Select Case reqlbl
+                            Case "Label2"
+                                Return "Welcome to the preinstallation environment for Windows Live OneCare Rewritten by Cobs." & Environment.NewLine & Environment.NewLine &
+                                    "Installation Notes:" & Environment.NewLine & Environment.NewLine &
+                                    "-If you're attempting a reinstall after a failed initial install, please delete %temp%\OCSetup" & Environment.NewLine &
+                                    "and c:\OneCare" & Environment.NewLine &
+                                    "-Windows XP is only supported with 2.5 discs on SP3." & Environment.NewLine &
+                                    "-Backup is only functional on V1.5 Gold discs." & Environment.NewLine & Environment.NewLine & Environment.NewLine &
+                                    "Requirements:" & Environment.NewLine &
+                                    "-Microsoft .Net 4.0" & Environment.NewLine &
+                                    "-Windows Vista or Windows XP 32-Bit (SP3)" & Environment.NewLine &
+                                    "-Windows Live ID Assistant Service" & Environment.NewLine &
+                                    "-Windows Live OneCare disc in optical drive or virtually mounted"
+                            Case Else
+                                Return errtxt
+                        End Select
+                    Case Else
+                        Return errtxt
+                End Select
+
+
+            Case "fr-fr"
+                Select Case formname
+                    Case "preinst"
+                        Select Case reqlbl
+                            Case "label2"
+                            Case Else
+                                Return errtxt
+                        End Select
+                    Case Else
+                        Return errtxt
+                End Select
+            Case Else
+                Return errtxt
+        End Select
+
+
+    End Function
+End Module
